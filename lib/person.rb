bf81d2415a915c779c0ae52d1e90046a0f17968a
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-21 18:18:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
   
   def initialize(properties)
     
-    properties.each {|key, value | self.send(#{key}=, value)}
+    properties.each {|key, value | self.send((#{key}=), value)}
     
   end 
   
